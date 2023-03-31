d6c9adc767dc674387f0475f9da845d51fa4a98f
alexis <aleperalta252@gmail.com>
2023-03-31 11:13:54 -0300

    routing

--- gestionAngular/src/app/app.module.ts
+++ gestionAngular/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+ 
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
